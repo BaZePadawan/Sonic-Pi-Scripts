7bf8281336982b057532b229dc990d8f0a664767
BaZePadawan <vitaljosephvista@gmail.com>
2018-02-02 15:40:29 -0800

    Add files via upload

---
+++ stranger things.rb
@@ -0,0 +1,21 @@
+# Welcome to Sonic Pi v2.11.1
+use_bpm 160
+live_loop :intro do
+  play :C4
+  sleep 0.50
+  play :E4
+  sleep 0.50
+  play :G4
+  sleep 0.50
+  play :B4
+  sleep 0.50
+  play :B4
+  sleep 0.50
+  play :G4
+  sleep 0.50
+  play :E4
+  sleep 0.50
+  play :C4
+  sleep 0.50
+end
+
